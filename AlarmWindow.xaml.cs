8c81e1a33be200856b2f7ff7073613e90414154b
trichoip <107303342+trichoip@users.noreply.github.com>
2023-07-22 19:47:39 +0700

    first commit

---
+++ AlarmWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Prn221_Count_Down_Clock
+{
+    /// <summary>
+    /// Interaction logic for AlarmWindow.xaml
+    /// </summary>
+    public partial class AlarmWindow : Window
+    {
+        private DispatcherTimer timer;
+        private DateTime alarmTime;
+
+        public AlarmWindow()
+        {
+            InitializeComponent();
+            StopButton.IsEnabled = false;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int hour = int.Parse(((ComboBoxItem)HourComboBox.SelectedItem).Content.ToString());
+            int minute = int.Parse(((ComboBoxItem)MinuteComboBox.SelectedItem).Content.ToString());
+            int second = int.Parse(((ComboBoxItem)SecondComboBox.SelectedItem).Content.ToString());
+            alarmTime = DateTime.Today + new TimeSpan(hour, minute, second);
+
+            TimeSpan timeRemaining = alarmTime - DateTime.Now;
+            if (timeRemaining.TotalMilliseconds > 0)
+            {
+                if (timer == null)
+                {
+                    timer = new DispatcherTimer();
+                    timer.Interval = timeRemaining;
+                    timer.Tick += Timer_Tick;
+                }
+
+                timer.Start();
+                MessageBox.Show("Alarm set for " + alarmTime.ToString("t"));
+                StopButton.IsEnabled = true;
+                StartButton.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("you cann't set time before now");
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer != null && timer.IsEnabled)
+            {
+                timer.Stop();
+            }
+            StopButton.IsEnabled = false;
+            StartButton.IsEnabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            MessageBox.Show("Alarm!");
+            StopButton.IsEnabled = false;
+            StartButton.IsEnabled = true;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Prn221_Count_Down_Clock
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private int totalSeconds;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            ResetTimer();
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputSecondsTextBox.Text, out int seconds))
+            {
+                totalSeconds = seconds;
+                if (timer == null)
+                {
+                    timer = new DispatcherTimer();
+                    timer.Interval = TimeSpan.FromSeconds(1);
+                    timer.Tick += Timer_Tick;
+                }
+
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number of seconds.");
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer != null && timer.IsEnabled)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetTimer();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            totalSeconds--;
+            UpdateTimeDisplay();
+
+            if (totalSeconds <= 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Countdown finished!");
+            }
+        }
+
+        private void UpdateTimeDisplay()
+        {
+            int hours = totalSeconds / 3600;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
+
+            TimerTextBlock.Text = $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+        }
+
+        private void ResetTimer()
+        {
+            totalSeconds = 0;
+            UpdateTimeDisplay();
+            if (timer != null && timer.IsEnabled)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void Alarm_Click(object sender, RoutedEventArgs e)
+        {
+            new AlarmWindow().Show();
+            this.Close();
+
+        }
+    }
+}
